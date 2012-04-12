a657607070cb96508cfe03c9d8d82cfaa8ad570d
Gregory <gamedevguy12@gmail.com>
2012-04-11 22:33:39 -0400

    Edited something.

--- python/main.py
+++ python/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 from sys import argv
 script, query = argv
+#You will need this module: http://googolplex.sourceforge.net/
 from google import search
 for url in search('(%r)' % query, stop=20):
 #for url in search('"',search, stop=3):
