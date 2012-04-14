83332226705313ffc3546ac5ba7122970e123ea8
Gregory <gamedevguy12@gmail.com>
2012-04-13 22:13:43 -0400

    Added error handling to main.py

--- python/main.py
+++ python/main.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python
+# Created by: TSF
+#WTFPL
 from sys import argv
-script, query = argv
+import sys
+selfname = sys.argv[0]
+try:
+   script, query = argv
+except ValueError:
+    print 'Syntax: %r "query"' % selfname
+    quit()
 #You will need this module: http://googolplex.sourceforge.net/
 from google import search
 for url in search('(%r)' % query, stop=20):
-#for url in search('"',search, stop=3):
-          print(url)
+                print(url)
+
+
+
+
